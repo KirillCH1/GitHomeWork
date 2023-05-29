9ddae7d57d743e47a729143c309ffc136d259af6
Kirill <Kirik.77@Yandex.ru>
2023-05-29 19:11:16 +0300

    Добавлено описание класса Laptop

--- Laptop.java
+++ Laptop.java
@@ -16,5 +16,34 @@ public class Laptop {
         this.colors = colors;
     }
 
+       public void printDescription (){
+        System.out.println(brand);
+        System.out.println("Оперативная память: " + ram + " Гб");
+        System.out.println("Жесткий диск: " + hdd + " Гб");
+        System.out.println("Операционная система: " + os);
+        System.out.println("Цвет: " + colors);
+    }
+
+        @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if(!(obj instanceof Laptop)){
+            return false;
+        }
+        int flag = 0;
+        Laptop laptop = (Laptop) obj;
+        if (brand.equals(laptop.brand) || brand.equals("")){ flag++; }
+        if(ram <= laptop.ram || ram == 0){ flag ++; }
+        if (hdd <= laptop.hdd || hdd == 0) { flag++; }
+        if (os.equals(laptop.os) || os.equals("")){ flag++; }
+        if (colors.equals(laptop.colors) || colors.equals("")){ flag++; }
+        if (flag == 5){
+            return true;
+        }
+        return false;
+    }
+
 
 }
